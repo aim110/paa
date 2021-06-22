25651a8e5016c860b236587450ebb330e9da94cb
aim110 <i.michailyukov@gmail.com>
2021-06-22 22:11:01 +0300

    added getting current price for orders, added current positions import

--- paa.py
+++ paa.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-import yfinance  # kinda docs: https://pypi.org/project/yfinance/
+import yfinance as yf  # kinda docs: https://pypi.org/project/yfinance/
 import pandas as pd  # https://pandas.pydata.org/docs/index.html
 pd.options.mode.chained_assignment = None  # default='warn' to make my excessive data copying sleep well
 
@@ -23,8 +23,8 @@ def get_month(row):
 
 def fetch_all_ticker_data(tickers):
     today = date.today()
-    start = date(year = today.year - 1, month = today.month - 1, day = today.day)
-    return yfinance.download(tickers, start=str(start), end=str(today), interval='1mo', group_by='ticker')
+    start = date(year = today.year - 1, month = today.month - 1, day = 1)
+    return yf.download(tickers, start=str(start), end=str(today), interval='1mo', group_by='ticker')
 
 
 def cleanup_ticker_data(d, lookback):
@@ -39,6 +39,9 @@ def cleanup_ticker_data(d, lookback):
 
 
 def BF(N, n, a):
+    """
+    calc bond fraction
+    """
     n1 = a*N/4.
     return (N - n) / (N - n1)
 
@@ -53,9 +56,52 @@ def print_sorted_mom_filtered(tickers, all_momentum_sorted):
         print("{}: {}".format(t, m))
 
 
+def get_last_prices(tickers_list):
+    data = yf.download(tickers=tickers_list, period='2h', interval='5m')
+    tail = data.dropna().tail(1)["Close"]
+    return {ticker: tail[ticker][0] for ticker in tail}
+
+
+def read_ticker_state(fn):
+    return pd.read_csv(fn, header=None, names=["ticker", "amount"], index_col="ticker")
+
+
+def prepare_orders(ticker_current, target_tickers, ticker_to_sum):
+    """
+    orders to buy and to sell
+    """
+    all_tickers = set(ticker_current.index) | set(target_tickers) 
+    last_price = get_last_prices(all_tickers)
+    #print("LP: {}".format(last_price))
+
+    orders = []
+    # BUY PART
+    for ticker in target_tickers:
+        #print("Ticker: {}".format(ticker))
+        try:
+            current_shares = ticker_current.at[ticker, "amount"]
+        except KeyError:
+            current_shares = 0
+        target_shares = ticker_to_sum[ticker] / last_price[ticker]
+        #print("curr: {}\ttarget_shares: {}".format(current_shares, target_shares))
+
+        amount = target_shares - current_shares 
+        order_type = 'buy' if amount > 0 else 'sell'
+        orders.append((order_type, ticker, amount))
+
+    # Sell shares that not in target but currently available
+    to_sell = set(ticker_current.index) - set(target_tickers)
+    for t in to_sell:
+        orders.append(('sell', t, 'all'))
+
+
+    return orders
+
+
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--amount', type=int, help="calculate shares according to that amount of money")
+    parser.add_argument('--amount', type=int, help="calculate shares according to that amount of money", required=True)
+    parser.add_argument('--current_fn', type=str, help="csv files with current positions", required=True)
     args = parser.parse_args()
     return args
 
@@ -63,11 +109,12 @@ def parse_args():
 def main():
     args = parse_args()
 
-    paa_risky = set(['SPY', 'IWM', 'QQQ', 'VGK', 'EWJ', 'EEM', 'VNQ', 'DBC', 'GLD', 'HYG', 'LQD', 'TLT']) # NB: SCZ looks quite interesting!!!
+    paa_risky = set(['VTI', 'IWM', 'QQQ', 'VGK', 'EWJ', 'EEM', 'VNQ', 'DBC', 'GLD', 'HYG', 'LQD', 'TLT']) # NB: SPY - original, I got VTI instead
     canary = set(['AGG', 'EEM'])
     iamcurious = set('EFV RWO IWN SCZ EFA IVE'.split()) # TBD: move to separate script for wondering
-    sectoral = set('VAW VOX VDE VFH VIS VGT VDC VNQ VPU VHT VCR'.split())  # energy communcation energy finance industrials tech staples realEstate utilities healthcare discretionary
-    safe = set(['IEF', 'SHY', 'LQD', 'TIPS', 'VGSH', 'STIP', 'VCSH', 'IAGG'])
+    sectoral = set('VAW VOX XLE VFH VIS VGT VDC VNQ VPU VHT VCR'.split())  # energy communcation energy finance industrials tech staples realEstate utilities healthcare discretionary
+    # is VDE better than XLE?
+    safe = set(['IEF', 'SHY', 'LQD', 'TIP', 'VGSH', 'VCSH', 'IAGG', "STIP"]) # STIP
 
     all_tickers = {*paa_risky, *canary, *safe, *iamcurious, *sectoral}
     df = fetch_all_ticker_data(all_tickers)
@@ -80,21 +127,20 @@ def main():
     # Paper strategy comparison: page 9
 
     ticker_mom = {}
-    ticker_last_price = {}
     for t in all_tickers:
         ticker_data = df[t].copy(deep=True)
         cleaned_data = cleanup_ticker_data(ticker_data, L)
 
         sma = SMA(cleaned_data, L)  # TBD: not sure if I am doing that correctly. Do I need 13 points for SMA12? Should I include p0 to SMA calculation?
         last_price = cleaned_data.tail(1).iloc[0]["Close"]
-        ticker_last_price[t] = last_price
-
         momentum = MOM(last_price, sma)
         ticker_mom[t] = momentum
 
     sorted_mom = sorted(ticker_mom.items(), key=lambda x: x[1])
     print("\nAll tickers momentum:")
-    print_sorted_mom_tickers(sorted_mom)
+    for t, mom in sorted_mom:
+        extra = ' ' if t not in paa_risky else '*'
+        print("{} {}: {}".format(extra, t, mom))
 
     positive_momentum_assets = 0
     for t in paa_risky:
@@ -109,21 +155,29 @@ def main():
     bf = BF(N, positive_momentum_assets, A)
     risky_asset_share = (1.0 - bf) / TOP
     print("\nShare of each risky asset: ", round(risky_asset_share, 2))
-    if args.amount:
-        asset_value = round(args.amount * risky_asset_share, 1)
-        print("Buy each asset on: {}$. Shares for equites:".format(asset_value))
-        for t, _ in top:
-            price = round(ticker_last_price[t], 1)
-            shares_amount = round(asset_value / price, 1)
-            print("\t`{}`: {}\tprice={}".format(t, shares_amount, price))
+    risky_target = round(args.amount * risky_asset_share, 1)
 
     print("\nBond fraction: ", round(bf, 2))
-    if args.amount:
-        print("Buy best safe asset on: {}$".format(round(args.amount * bf, 1)))
 
     print("\nSafe assets momentum:")
     print_sorted_mom_filtered(safe, sorted_mom)
+    print("Buy best safe asset on: {}$".format(round(args.amount * bf, 1)))
+    top_safe = [t for t, m in sorted_mom if t in safe][-1]
+    safe_target = round(args.amount * bf, 1)
+
+    if args.amount:
+        risky_top = [t for t, _ in top]
+        targets = {**{top_safe: safe_target}, **{t: risky_target for t in risky_top}}
+
+        orders = prepare_orders(read_ticker_state(args.current_fn), [top_safe] + risky_top, targets)
+
+        for ot, ticker, amount in sorted(orders, key=lambda x: x[0]):
+            val = round(amount, 1) if type(amount) != str else amount
+            print("\t{}\t{}\t{}".format(ot.upper(), ticker, val))
 
 
+
+# TBD: add descr for ticker at `buy` section yf.Ticker("VAW").info["longName"]
+# TBD: move to args `risky assets`, `save assets` (or to yaml config)
 if __name__ == '__main__':
     main()
